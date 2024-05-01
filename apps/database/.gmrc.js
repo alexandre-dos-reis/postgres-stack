cd47a171919bd2d1ff62bf8e9e66782cd20aae0c
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2024-05-01 14:53:53 +0200

    added graphile migrate

---
+++ apps/database/.gmrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  pgSettings: {
+    // "search_path": "app_public,app_private,app_hidden,public",
+  },
+  placeholders: {
+    ":ANON_ROLE": "!ENV",
+    ":PERSON_ROLE": "!ENV",
+    ":PRIVATE_SCHEMA": "!ENV",
+    ":FRONT_SCHEMA": "!ENV",
+    ":ADMIN_SCHEMA": "!ENV",
+  },
+  afterReset: [
+    // "afterReset.sql",
+    // { "_": "command", "command": "graphile-worker --schema-only" },
+  ],
+  afterAllMigrations: [
+    // {
+    //   "_": "command",
+    //   "shadow": true,
+    //   "command": "if [ \"$IN_TESTS\" != \"1\" ]; then ./scripts/dump-db; fi",
+    // },
+  ],
+
+  afterCurrent: [
+    // {
+    //   "_": "command",
+    //   "shadow": true,
+    //   "command": "if [ \"$IN_TESTS\" = \"1\" ]; then ./scripts/test-seed; fi",
+    // },
+  ],
+
+  /****************************************************************************\
+  ***                                                                        ***
+  ***         You probably don't want to edit anything below here.           ***
+  ***                                                                        ***
+  \****************************************************************************/
+
+  /*
+   * manageGraphileMigrateSchema: if you set this false, you must be sure to
+   * keep the graphile_migrate schema up to date yourself. We recommend you
+   * leave it at its default.
+   */
+  // "manageGraphileMigrateSchema": true,
+
+  /*
+   * migrationsFolder: path to the folder in which to store your migrations.
+   */
+  // migrationsFolder: "./migrations",
+
+  "//generatedWith": "2.0.0-rc.2",
+};
--- devenv.nix
+++ devenv.nix
@@ -1,18 +1,34 @@
 { pkgs, lib, config, inputs, ... }: let 
+
+  APP_ENV_DEV = "development";
+  APP_ENV_PROD = "production";
+  APP_ENV_CI = "ci";
+  APP_ENV_TEST = "test";
+
+  APP_ANON_ROLE = "anon";
+  APP_PERSON_ROLE = "person";
+
+  APP_PRIVATE_SCHEMA = "app_private";
+  APP_FRONT_SCHEMA = "app_front";
+  APP_ADMIN_SCHEMA = "app_admin";
+
+  # Postgres Database
   DB_DATABASE = "mydb";
   DB_PORT = 9876;
   DB_OWNER_USER = "alexandre";
   DB_OWNER_PASS = "alexandre";
 
+  # Postgrest api
   PGRST_PORT = 3333;
   PGRST_DB_ANON_ROLE = "alexandre";
 
-  OPENAPI_API_PORT = PGRST_PORT;
+  # OpenApi
   OPENAPI_UI_PORT = 3434;
-
+  OPENAPI_API_PORT = PGRST_PORT;
   in {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env = {
+    APP_ENV = APP_ENV_DEV;
+  };
 
   # https://devenv.sh/packages/
   packages = with pkgs; [
@@ -21,27 +37,44 @@
     watchexec
   ];
 
+  scripts = {
+    # Graphile Migrate
+    gm.exec = ''
+      cd apps/database && \
+        DATABASE_URL="postgres://${DB_OWNER_USER}:${DB_OWNER_PASS}@localhost:${toString DB_PORT}/${DB_DATABASE}" \
+        SHADOW_DATABASE_URL="postgres://${DB_OWNER_USER}:${DB_OWNER_PASS}@localhost:${toString DB_PORT}/${DB_DATABASE}_shadow" \
+        ROOT_DATABASE_URL="postgres://${DB_OWNER_USER}:${DB_OWNER_PASS}@localhost:${toString DB_PORT}/postgres" \
+        ANON_ROLE=${APP_ANON_ROLE} \
+        PERSON_ROLE=${APP_PERSON_ROLE} \
+        PRIVATE_SCHEMA=${APP_PRIVATE_SCHEMA} \
+        FRONT_SCHEMA=${APP_FRONT_SCHEMA} \
+        ADMIN_SCHEMA=${APP_ADMIN_SCHEMA} \
+        ${pkgs.bun}/bin/bunx --bunx graphile-migrate@next $1
+    '';
+  };
+
   processes = {
     postgrest.exec = ''
       PGRST_DB_URI="postgres://${DB_OWNER_USER}:${DB_OWNER_PASS}@localhost:${toString DB_PORT}/${DB_DATABASE}" \
-      PGRST_SERVER_PORT="${toString PGRST_PORT}" \
-      PGRST_DB_ANON_ROLE="${PGRST_DB_ANON_ROLE}" \
-      PGRST_OPEN_API_MODE="ignore-privileges" \
+        PGRST_SERVER_PORT="${toString PGRST_PORT}" \
+        PGRST_OPEN_API_MODE="ignore-privileges" \
+        PGRST_DB_ANON_ROLE="${PGRST_DB_ANON_ROLE}" \
+        PGRST_DB_SCHEMAS="" \
         postgrest
     '';
 
     openapi-ui.exec = ''
       cd apps/openapi-ui && \
-      VITE_OPENAPI_API_PORT=${toString OPENAPI_API_PORT} \
+        VITE_OPENAPI_API_PORT=${toString PGRST_PORT} \
         ${pkgs.bun}/bin/bun run dev --port ${toString OPENAPI_UI_PORT}
     '';
 
     openapi-codegen.exec = ''
       cd packages/openapi-codegen && \
         ${pkgs.watchexec}/bin/watchexec -- \
-          ${pkgs.bun}/bin/bunx --bunx @hey-api/openapi-ts \
-          -i http://localhost:${toString PGRST_PORT} \
-          -o src
+        ${pkgs.bun}/bin/bunx --bunx @hey-api/openapi-ts \
+        -i http://localhost:${toString PGRST_PORT} \
+        -o src
     '';
   };
 
